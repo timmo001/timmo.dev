501864e87de85969410a6c5c2c55b6e58328229c
Aidan Timson <contact@timmo.xyz>
2022-08-20 13:13:54 +0100

    Initial

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,25 @@
+import { h } from "preact";
+import Styles from "./styles.module.scss";
+
+function Footer() {
+  return (
+    <footer className={Styles.footer}>
+      &copy; {new Date().getFullYear()} Aidan Timson
+      <br />
+      <br />
+      <small className={Styles.byline}>
+        Built and designed by Aidan Timson
+        <br />
+        <br />
+        Source avaliable on{" "}
+        <a
+          href="https://github.com/timmo001?tab=repositories&q=timmo-cms"
+          target="_blank"
+        >
+          GitHub
+        </a>
+      </small>
+    </footer>
+  );
+}
+export default Footer;
