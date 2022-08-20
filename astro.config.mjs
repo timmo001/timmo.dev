f2f264cb1e3870b76a6a79bf3883d72151b3f77f
Aidan Timson <contact@timmo.xyz>
2022-08-20 16:28:45 +0100

    Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
-
-import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://timmo.dev",
+  adapter: vercel(),
   experimental: {
     integrations: true,
   },
-  integrations: [preact({ compat: true }), sitemap(), robotsTxt()],
+  integrations: [preact(), sitemap(), robotsTxt()],
+  output: "server",
+  site: "https://timmo.dev",
 });
