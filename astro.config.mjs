64a1c2dcb1d80cb78f1c710dbcfcd0906dc6f74f
Aidan Timson <contact@timmo.xyz>
2022-08-21 16:47:07 +0100

    Edge not serveless

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
