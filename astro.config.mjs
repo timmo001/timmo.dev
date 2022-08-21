76ae1072b6fcc61a04420b408fa1bac3495d0153
Aidan Timson <contact@timmo.xyz>
2022-08-21 17:07:37 +0100

    Remove vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,12 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel(),
   experimental: {
     integrations: true,
   },
   integrations: [preact(), sitemap(), robotsTxt()],
-  output: "server",
   site: "https://timmo.dev",
 });
