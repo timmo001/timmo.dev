cf943b7bb0a2760eb558e4589b3beb782a221728
Aidan Timson <contact@timmo.xyz>
2023-08-13 20:48:06 +0100

    Remove old

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   experimental: {
-    integrations: true,
     viewTransitions: true,
   },
   integrations: [preact(), robotsTxt(), sitemap()],
