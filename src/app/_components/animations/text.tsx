c7ad325a37185df13cf87141dcb384c3606bed7a
Aidan Timson <aidan@timmo.dev>
2024-08-02 22:43:45 +0100

    Content

--- src/app/_components/animations/text.tsx
+++ src/app/_components/animations/text.tsx
@@ -1,11 +1,26 @@
 "use client";
 import { motion } from "framer-motion";
 
-export function TextGrab({
+export function TextFadeInUp({
   children,
 }: Readonly<{ children: React.ReactNode }>): JSX.Element {
   return (
     <motion.text
+      initial={{ opacity: 0, y: 15 }}
+      transition={{ duration: 0.5 }}
+      whileInView={{ opacity: 1, y: 0 }}
+    >
+      {children}
+    </motion.text>
+  );
+}
+
+export function TextFadeInUpGrab({
+  children,
+}: Readonly<{ children: React.ReactNode }>): JSX.Element {
+  return (
+    <motion.text
+      className="select-none"
       drag
       dragConstraints={{
         top: -10,
@@ -15,6 +30,9 @@ export function TextGrab({
       }}
       dragSnapToOrigin
       style={{ cursor: "grab" }}
+      initial={{ opacity: 0, y: 15 }}
+      transition={{ duration: 0.5 }}
+      whileInView={{ opacity: 1, y: 0 }}
     >
       {children}
     </motion.text>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,59 @@
-import { TextGrab } from "~/app/_components/animations/text";
+import {
+  TextFadeInUp,
+  TextFadeInUpGrab,
+} from "~/app/_components/animations/text";
 
 export default function Home() {
   return (
     <>
-      <header
-        className="max-w-screen flex min-h-screen w-full select-none flex-col items-start justify-end bg-contain bg-center bg-no-repeat p-8"
-        style={{
-          backgroundImage: "url('/logo-square-wide-nobackground.png')",
-        }}
-      >
-        <div className="flex flex-wrap items-baseline justify-start text-start">
-          <TextGrab>
-            <span className="me-6 text-4xl font-bold leading-none tracking-tight text-white sm:text-[6rem]">
-              Aidan Timson
-            </span>
-          </TextGrab>
-          <TextGrab>
-            <span className="text-nowrap text-[0.6rem] font-bold leading-none tracking-normal sm:text-[2rem]">
-              <span className="text-indigo-500">Timmo</span> /{" "}
-              <span className="text-indigo-500">@timmo001</span>
-            </span>
-          </TextGrab>
+      <header className="h-screen w-full p-4">
+        <div
+          className="max-w-screen flex h-full w-full flex-col items-start justify-end bg-contain bg-center bg-no-repeat p-16"
+          style={{
+            backgroundImage: "url('/logo-wide-nobackground.png')",
+          }}
+        >
+          <div className="flex flex-wrap items-baseline justify-start text-start">
+            <TextFadeInUpGrab>
+              <span className="me-6 text-8xl font-bold leading-none tracking-tight text-white">
+                Aidan Timson
+              </span>
+            </TextFadeInUpGrab>
+            <TextFadeInUpGrab>
+              <span className="text-nowrap text-4xl font-bold leading-none tracking-normal">
+                <span className="text-indigo-500">Timmo</span> /{" "}
+                <span className="text-indigo-500">@timmo001</span>
+              </span>
+            </TextFadeInUpGrab>
+          </div>
+          <TextFadeInUpGrab>
+            <h2 className="mt-4 text-wrap text-3xl font-semibold leading-snug tracking-tight text-white">
+              Software Developer
+            </h2>
+          </TextFadeInUpGrab>
         </div>
-        <TextGrab>
-          <h2 className="text-wrap text-xs font-extrabold leading-snug tracking-tight text-white sm:text-[4rem]">
-            Software Developer
-          </h2>
-        </TextGrab>
       </header>
 
       <div className="container flex flex-col items-center justify-start gap-12 px-4 py-16">
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8"></div>
+        <div className="grid-row grid gap-x-4 gap-y-24 sm:grid-cols-1 md:grid-cols-2">
+          <TextFadeInUpGrab>
+            <h3 className="text-2xl font-bold leading-snug tracking-tight text-white">
+              About Me
+            </h3>
+          </TextFadeInUpGrab>
+          <TextFadeInUp>
+            <p className="text-lg font-normal leading-relaxed tracking-normal text-white">
+              I am a developer with a passion for creating and maintaining
+              software. I have experience in full-stack development and have
+              worked on a variety of projects.
+              <br />
+              <br />
+              I'm a tinkerer, who has a vast interest for technology, automation
+              and software. I'm always looking to improve my skills and learn
+              new things.
+            </p>
+          </TextFadeInUp>
+        </div>
       </div>
     </>
   );
