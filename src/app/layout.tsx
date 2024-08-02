3f06f9df0a284dc272fe8ca380545faf54e1ee0a
Aidan Timson <contact@timmo.xyz>
2024-08-02 16:56:19 +0100

    Setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,22 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+      <body className="min-h-screen bg-background font-sans antialiased">
+        <main className="flex min-h-screen flex-col items-center justify-start bg-gradient-to-b from-violet-900 to-[#020617] text-white">
+          <header
+            className="flex min-h-[48rem] w-full items-end justify-start bg-cover bg-center p-8"
+            style={{
+              backgroundImage: "url('/logo-square-wide-nobackground.png')",
+            }}
+          >
+            <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+              Aidan Timson (
+              <span className="text-[hsl(280,100%,70%)]">Timmo</span>)
+            </h1>
+          </header>
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Aidan Timson (<span className="text-[hsl(280,100%,70%)]">Timmo</span>)
-        </h1>
+    <>
+      <div className="container flex flex-col items-center justify-start gap-12 px-4 py-16">
+        <h2 className="text-2xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          Full-Stack Software Developer
+        </h2>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8"></div>
       </div>
-    </main>
+    </>
   );
 }
