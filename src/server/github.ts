30fb6df8e19dd288bda711c205cee33d4bb5ddc0
Aidan Timson <contact@timmo.xyz>
2024-08-04 15:01:19 +0100

    Use gql in ts

--- src/server/github.ts
+++ src/server/github.ts
@@ -1,5 +1,4 @@
 "use server";
-import { promises as fs } from "fs";
 import { Octokit } from "octokit";
 
 import { User } from "~/types/github/user";
@@ -7,10 +6,67 @@ import { User } from "~/types/github/user";
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
 export async function getUserData(user: string): Promise<User> {
-  const query = await fs.readFile(
-    `${process.cwd()}/src/lib/graphql/user.graphql`,
-    "utf-8",
-  );
+  const query = `query ($login: String!) {
+  user(login: $login) {
+    id
+    name
+    avatarUrl
+    bio
+    email
+    login
+    contributionsCollection {
+      totalCommitContributions
+      totalIssueContributions
+      totalPullRequestContributions
+      totalPullRequestReviewContributions
+    }
+    followers {
+      totalCount
+    }
+    following {
+      totalCount
+    }
+    repositories(
+      first: 100
+      isFork: false
+      orderBy: { field: UPDATED_AT, direction: DESC }
+    ) {
+      totalCount
+      totalDiskUsage
+      nodes {
+        name
+        languages(first: 100, orderBy: { field: SIZE, direction: DESC }) {
+          edges {
+            size
+            node {
+              name
+              color
+            }
+          }
+        }
+      }
+    }
+    starredRepositories {
+      totalCount
+    }
+    socialAccounts(first: 100) {
+      nodes {
+        displayName
+        provider
+        url
+      }
+    }
+    status {
+      message
+      emoji
+      emojiHTML
+    }
+    watching {
+      totalCount
+    }
+  }
+}`;
+
   try {
     return await octokit.graphql<User>(query, {
       login: user,
